6146cfba0cb29e76cad61b18bf4adc0ff99a51df
Rui Jiang <rui.jiang.dev@gmail.com>
2021-02-09 06:03:42 -0800

    Question 753

---
+++ rj-app/src/test/java/com/myrj/app/hard/Question753Test.java
@@ -0,0 +1,12 @@
+package com.myrj.app.hard;
+
+import org.junit.Test;
+
+public class Question753Test {
+  Question753 solution = new Question753();
+
+  @Test
+  public void test1() {
+    System.out.println(solution.crackSafe(2, 2));
+  }
+}
